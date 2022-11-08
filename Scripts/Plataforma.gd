a4c2e58184be2bd529f883e3de2fe9c44831bc04
pablogiovannone <pablolaacad3@hotmail.com>
2022-11-08 19:30:21 -0300

    Se agregaron plataformas y lógica de las mismas

---
+++ Scripts/Plataforma.gd
@@ -0,0 +1,18 @@
+class_name Plataforma
+extends StaticBody2D
+
+
+export var speed = 300
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	position.y -= speed * delta
+	fuera_de_pantalla()
+
+func fuera_de_pantalla():
+	if position.y < 50:
+		queue_free()
---
+++ Scripts/PlatformsSpawner.gd
@@ -0,0 +1,28 @@
+extends Node
+
+const SPECIAL_PLATFORM_CHANCE = 50
+
+export(Array) var platforms
+
+func _ready():
+	_spawn_platforms()
+	pass
+	
+func _spawn_platforms():
+	randomize()
+	var index
+	var new_platform = platforms[0]
+	
+	if rand_range(0,100) > 100 - SPECIAL_PLATFORM_CHANCE:
+		index = int(rand_range(1, platforms.size()-1))
+		new_platform = platforms[index]
+		
+	new_platform = new_platform.instance()
+	
+	new_platform.position.x = rand_range(0, get_viewport().size.x)
+	new_platform.position.y = get_viewport().size.y
+	add_child(new_platform)
+	
+
+func _on_Timer_timeout():
+	_spawn_platforms()
