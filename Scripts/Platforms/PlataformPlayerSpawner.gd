c3b241016733eee30dad931220c3d25f90da62fd
Francisco Alcalde <francisco@alcalde.com.ar>
2022-11-23 20:50:07 -0300

    PlatformPlayerSpawn espawnea el jugador a la mitad de la pantalla

--- Scripts/Platforms/PlataformPlayerSpawner.gd
+++ Scripts/Platforms/PlataformPlayerSpawner.gd
@@ -4,27 +4,35 @@ extends Platform
 
 signal spawn_player(position)
 
-
 onready var anim_player = $AnimationPlayer
 onready var player_sprite = $PlayerSpawn
-
+var spawned = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim_player.play('COUNTDOWN')
-	print('_ready')
-
-
-func _init():
-	print('_init')
+	anim_player.play('SPAWNING')
 
 
 func _enter_tree():
-	print('_enter_tree')
 	GameEvents.player_spawning()
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == 'COUNTDOWN':
-		emit_signal('spawn_player', player_sprite.global_position)
-		GameEvents.spawn_player(player_sprite.global_position)
+		spawn_player()
+
+
+func _process(_delta):
+	var altura_pantalla = get_viewport_rect().size.y
+	var spawn_height = altura_pantalla / 2
+	
+	if position.y == spawn_height and spawned == false:
+		spawn_player()
+		spawned = true
+
+
+func spawn_player():
+	emit_signal('spawn_player', player_sprite.global_position)
+	GameEvents.spawn_player(player_sprite.global_position)
+	anim_player.stop()
+	player_sprite.visible = false
