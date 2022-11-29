22fb53606c44eb1d87604821663ad95863eba8f3
Francisco Alcalde <francisco@alcalde.com.ar>
2022-11-28 21:01:12 -0300

    Agregada escena de jugador con RigidBody

---
+++ Scripts/RPlayer.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+var move_input = 0
+var move_input_str = 300
+
+
+func _process(_delta):
+	move_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+
+func _integrate_forces(state):
+	var _velocity = state.get_linear_velocity()
+	var desired_vel = Vector2(move_input * move_input_str, _velocity.y)
+	state.set_linear_velocity(desired_vel)
