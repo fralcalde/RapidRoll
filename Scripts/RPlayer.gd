36a01ad0564c8cee01e66bd23a4155017cb43a88
Francisco Alcalde <francisco@alcalde.com.ar>
2023-01-03 17:33:21 -0300

    Added angular velocity to player

--- Scripts/RPlayer.gd
+++ Scripts/RPlayer.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var move_input = 0
 var move_input_str = 300
+var angular_speed = 2
 
 
 func _process(_delta):
@@ -12,3 +13,4 @@ func _integrate_forces(state):
 	var _velocity = state.get_linear_velocity()
 	var desired_vel = Vector2(move_input * move_input_str, _velocity.y)
 	state.set_linear_velocity(desired_vel)
+	state.angular_velocity += angular_speed * move_input
